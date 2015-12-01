04da11bcff9345a83543de3174af4710539f9eac
Francisco Charte Ojeda <francisco@fcharte.com>
2015-12-01 11:50:50 +0100

    Adds chapter 8 files

---
+++ Chapter8/DOM.xml.php
@@ -0,0 +1,11 @@
+<?php
+<object class="Page10" name="Page10" baseclass="Page">
+  <property name="Animations">a:0:{}</property>
+  <property name="Background"></property>
+  <property name="Caption">Page10</property>
+  <property name="Height">484</property>
+  <property name="HiddenFields">a:0:{}</property>
+  <property name="Name">Page10</property>
+  <property name="Width">781</property>
+</object>
+?>
